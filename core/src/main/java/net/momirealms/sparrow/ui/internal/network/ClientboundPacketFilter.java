292831018ff23c979e7a5f2fb6acbfb9d8b22e90
Catnies <catnies@foxmail.com>
2026-08-25 23:31:37 +0800

    pref: lambda 只会实现抽象方法, 屏蔽声明改为抽象以免漏写静默失效.

--- core/src/main/java/net/momirealms/sparrow/ui/internal/network/ClientboundPacketFilter.java
+++ core/src/main/java/net/momirealms/sparrow/ui/internal/network/ClientboundPacketFilter.java
@@ -12,9 +12,7 @@ public interface ClientboundPacketFilter {
      * @param packetIds 当前服务端的包 ID 注册表
      * @return 要拦下的包 ID
      */
-    default int[] suppressedPacketIds(@NotNull PacketIdRegistry packetIds) {
-        return new int[0];
-    }
+    int[] suppressedPacketIds(@NotNull PacketIdRegistry packetIds);
 
     /**
      * 判断当前 Window 是否要拦下这个 NMS 包对象.
@@ -22,5 +20,7 @@ public interface ClientboundPacketFilter {
      * @param packet 即将发送的原版 NMS 客户端包
      * @return 要拦下时返回 true, 否则让原版照常发送
      */
-    boolean suppresses(@NotNull Object packet);
+    default boolean suppresses(@NotNull Object packet) {
+        return false;
+    }
 }
