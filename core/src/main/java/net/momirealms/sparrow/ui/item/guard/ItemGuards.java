33bf29f3bca2c5cf90b3a74bae86f88918f08e70
Catnies <catnies@foxmail.com>
2026-08-27 08:21:24 +0800

    pref: 优化 ThrottleGuard 在单实例下的表现.

--- core/src/main/java/net/momirealms/sparrow/ui/item/guard/ItemGuards.java
+++ core/src/main/java/net/momirealms/sparrow/ui/item/guard/ItemGuards.java
@@ -5,6 +5,8 @@ import net.momirealms.sparrow.ui.item.click.ItemInteraction;
 import org.bukkit.GameMode;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.concurrent.TimeUnit;
+
 public final class ItemGuards {
 
     private ItemGuards() {
@@ -44,6 +46,6 @@ public final class ItemGuards {
      */
     @NotNull
     public static ItemGuard<ItemClick> throttle(long intervalMillis) {
-        return new ThrottleGuard(intervalMillis, System::currentTimeMillis);
+        return new ThrottleGuard(TimeUnit.MILLISECONDS.toNanos(intervalMillis), System::nanoTime);
     }
 }
